26cafe03e1e3e6211f02ff19805853b7afa6a032
sumanth <sumanthnagireddi@gmail.com>
2025-12-22 23:54:24 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:4200',
+      'https://sumanthnagireddi-interview.web.app/',
+      'https://sumanthnagireddi1.web.app/',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
